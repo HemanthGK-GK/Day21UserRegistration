0c5e7a71bc99f04c4679f3fb547f2a37997de606
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-13 13:24:09 +0530

    Hemanth : Valid Password Must contain minimum 8 characters

---
+++ UC5Password/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UC5Password
+{
+    class Program
+    {
+        class Password
+        {
+            public string RegexPassword = "^[0-9a-zA-Z]{8,}$";
+
+
+            public bool Validate(string password)
+            {
+                return Regex.IsMatch(password, RegexPassword);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string password;
+            bool result;
+            Password obj = new Password();
+
+            Console.WriteLine("Enter password");
+            password = Console.ReadLine();
+
+
+            result = obj.Validate(password);
+            Console.WriteLine(result);
+
+        }
+    }
+}
