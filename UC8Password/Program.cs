395717a3f9764ccd9dba9199752796250c62bc04
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-13 13:50:17 +0530

    Hemanth : Valid Password Must contain Exactly onr special Character

---
+++ UC8Password/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UC8Password
+{
+    class Program
+    {
+        class Password
+        {
+            public string RegexPassword = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-])[0-9a-zA-Z]{8,}$";
+
+
+            public bool Validate(string password)
+            {
+                return Regex.IsMatch(password, RegexPassword);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string password;
+            bool result;
+            Password obj = new Password();
+
+            Console.WriteLine("Enter password ");
+            Console.WriteLine("It Must Contain 1 Numeric value");
+            Console.WriteLine("It Must Contain 1 UpperCase letter");
+            Console.WriteLine("It Must Contain exact 1 Special Character");
+
+            password = Console.ReadLine();
+
+
+            result = obj.Validate(password);
+            Console.WriteLine(result);
+
+        }
+    }
+}
