4ea64e7cb25ed84825c0f9deadb8ff7253fcd215
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-13 13:20:15 +0530

    Hemanth :Program to get a Valid Phone Number

---
+++ UC4RegExPhoneNum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UC4RegExPhoneNum
+{
+    class Program
+    {
+        class Phone
+        {
+            public string RegexPhone = "^[9]{1}[1]{1}[ ]{1}[0-9]{10}$";
+           
+
+            public bool Validate(string Phone)
+            {
+                return Regex.IsMatch(Phone, RegexPhone);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string Phone;
+            bool result;
+            Phone obj = new Phone();
+
+            Console.WriteLine("Enter Phone number");
+            Phone = Console.ReadLine();
+
+
+            result = obj.Validate(Phone);
+            Console.WriteLine(result);
+
+        }
+    }
+}
