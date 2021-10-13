f26e81c04a11f839a01ad43b3b0bc0cd2201aff8
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-13 13:25:58 +0530

    Hemanth : Valid Password Must contain 1 upper case Character

---
+++ UC6Password/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UC6Password
+{
+    class Program
+    {
+        class Password
+        {
+            public string RegexPassword = "^(?=.*[A-Z])+[0-9a-zA-Z]{7,}$";
+
+
+            public bool Validate(string password)
+            {
+                return Regex.IsMatch(password, RegexPassword);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string password;
+            bool result;
+            Password obj = new Password();
+
+            Console.WriteLine("Enter password");
+            password = Console.ReadLine();
+
+
+            result = obj.Validate(password);
+            Console.WriteLine(result);
+
+        }
+    }
+}
