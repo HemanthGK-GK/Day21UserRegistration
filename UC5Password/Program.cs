df9e7b21d279e621ce43c3115a44feab7b645180
HemanthGK-GK <71811920+HemanthGK-GK@users.noreply.github.com>
2021-10-13 14:46:13 +0530

    Password Contain 8 Letters

--- UC5Password/Program.cs
+++ UC5Password/Program.cs
@@ -12,22 +12,14 @@ namespace UC5Password
 
             public bool Validate(string password)
             {
-                return Regex.IsMatch(password, RegexPassword);
+                if(password == null)
+            {
+                throw new UserRegistrationCustomException(UserRegistrationCustomException.ExceptionsType.INVALID_MESSAGE, "Invalid Password");
+            }
+            return Regex.IsMatch(password, Regex_PSWD);
             }
         }
-        static void Main(string[] args)
-        {
-            string password;
-            bool result;
-            Password obj = new Password();
-
-            Console.WriteLine("Enter password");
-            password = Console.ReadLine();
-
-
-            result = obj.Validate(password);
-            Console.WriteLine(result);
-
-        }
+       
+        
     }
 }
