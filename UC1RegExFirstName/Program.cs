fc739a47c3f574a9509095eac658f12202ab616f
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-13 13:13:26 +0530

    Hemanth:Program to get Valid FirstName

---
+++ UC1RegExFirstName/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UC1RegExFirstName
+{
+    class Program
+    {
+        class ChkFirstName
+        {
+            public string RegexFirstName = "^[A-Z]{1}[a-z]{2,}$";
+            public bool Validate(string firstname)
+            {
+                return Regex.IsMatch(firstname, RegexFirstName);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string FirstName;
+            bool result;
+            ChkFirstName obj = new ChkFirstName();
+
+            Console.WriteLine("Enter FirstName");
+            FirstName = Console.ReadLine();
+
+
+            result = obj.Validate(FirstName);
+            Console.WriteLine(result);
+        }
+    }
+}
